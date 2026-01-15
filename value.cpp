d5e46ed9ecfb30a7f84ed2e56c55e868b40857fc
MatthesF <matthes.fogtmann@gmail.com>
2026-01-15 19:35:37 +0100

    Move demo main into separate file

--- value.cpp
+++ value.cpp
@@ -1,6 +1,5 @@
 #include "value.h"
 #include <set>
-#include <iostream>
 
 std::shared_ptr<Value> operator+(const std::shared_ptr<Value>& lhs, const std::shared_ptr<Value>& rhs)
 {
@@ -58,31 +57,4 @@ void backprop(const std::shared_ptr<Value>& root){
     for (auto it{nodes.rbegin()}; it != nodes.rend(); ++it){
         (*it)->backward();
     }
-}
-
-int main() {
-    // 1. Inputs
-    auto a = std::make_shared<Value>(-4.0);
-    auto b = std::make_shared<Value>(2.0);
-
-    // 2. Math: d = a * (a + b) + b
-    // Intermediate: c = a + b
-    auto c = a + b;
-    // Result: d = a * c + b
-    auto d = a * c + b;
-
-    // 3. Backprop
-    backprop(d);
-
-    // 4. Print Results
-    // Expected: d = -4 * (-2) + 2 = 10
-    std::cout << "d value: " << d->value << " (Expected: 10)" << std::endl;
-    
-    // Expected: da = c + a*1 = -2 + (-4) = -6
-    std::cout << "a grad: "  << a->grad  << " (Expected: -6)" << std::endl;
-    
-    // Expected: db = a*1 + 1 = -4 + 1 = -3
-    std::cout << "b grad: "  << b->grad  << " (Expected: -3)" << std::endl;
-
-    return 0;
 }
